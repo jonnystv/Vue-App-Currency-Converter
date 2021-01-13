dca283fe7137cf720c37ea440954d7dfc487b77e
jonnystv <jonathan@darefoundation.com>
2021-01-13 00:25:38 +0000

    Loots of trouble retrieving exchange rates and getting vue devtools to work

--- src/app.js
+++ src/app.js
@@ -3,6 +3,19 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: "#app",
+        data: {
+            currencies: [],
+        },
+        mounted() {
+            this.getCurrencies()
+        },
+        methods: {
+            getCurrencies: function() {
+                fetch("https://api.exchangeratesapi.io/latest") 
+                .then(res => res.json()) 
+                .then(this.currencies => this.currencies = currencies)
+            },
+        },
     });
 });
 
